b877f45ef71ab87e5569dac793137a8fb3157f24
Filipp Tarkovich <70907211+fill-tarkovich@users.noreply.github.com>
2023-03-30 15:59:36 +0300

    create mongodb setup

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+import connectDB from './mongodb/connect.js';
+
 dotenv.config();
 
 const app = express();
@@ -11,3 +13,13 @@ app.use(express.json({ limit: '50mb' }));
 app.get('/', async (req, res) => {
     res.send('Hello from DALL-E');
 })
+
+const startServer = async () => {
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8800, () => console.log('Server has started on port http://localhost:8800'))
+    } catch (error) {
+        console.log(error);
+    }
+}
+startServer();
